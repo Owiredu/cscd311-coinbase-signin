819c42be0cc69e2b6ff8021f0884d60282d396dc
Owiredu Nana Kofi <nanakofiowiredu@gmail.com>
2021-05-21 22:37:57 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Menu from './Menu';
+import Login from './Login';
+
+function App() {
+  return (
+    <div class="page-container">
+      <Menu />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
